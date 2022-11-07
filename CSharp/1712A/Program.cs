889ae97f80a04cc1cce0d4a5f1fc9e32a03fb3c9
Mihadul <mehad65@gmail.com>
2022-11-07 15:19:56 +0600

    1

--- CSharp/1712A/Program.cs
+++ CSharp/1712A/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+
+namespace CD_1712A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+            for (int i = t; i > 0; i--)
+            {
+                int[] a = Array.ConvertAll(Console.ReadLine().Split(), Convert.ToInt32);
+                int[] b = Array.ConvertAll(Console.ReadLine().Split(), Convert.ToInt32);
+                int ans = 0;
+                Array.ForEach(b, i => { if (i <= a[1] && Array.IndexOf(b, i) >= a[1]) ans++; });
+                Console.WriteLine(ans);
+            }
+        }
+    }
+}
